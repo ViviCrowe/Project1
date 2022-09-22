5557e45f7a9a44a23484944633c47a0a7dd5adeb
ViviCrowe <vivian.fischer96@gmail.com>
2022-09-22 21:11:32 +0200

    Task 2 done

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Data.Common;
 
 namespace Project1
 {
@@ -8,6 +9,10 @@ namespace Project1
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D ball;
+        private Vector2 ballLocation;
+        private float ballRotation;
+        private SpriteFont _font;
 
         public Game1()
         {
@@ -19,6 +24,8 @@ namespace Project1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            ballLocation = new Vector2(GraphicsDevice.Viewport.Width/2f, GraphicsDevice.Viewport.Height / 2f);
+            ballRotation = 0.0f;
 
             base.Initialize();
         }
@@ -26,6 +33,8 @@ namespace Project1
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            ball = Content.Load<Texture2D>("ball");
+            _font = Content.Load<SpriteFont>("Arial");
 
             // TODO: use this.Content to load your game content here
         }
@@ -35,7 +44,21 @@ namespace Project1
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            ballRotation += 0.1f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                ballLocation.Y -= 10;
+            } else if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                ballLocation.Y += 10;
+            } else if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                ballLocation.X += 10;
+            } else if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                ballLocation.X -= 10;
+            }
 
             base.Update(gameTime);
         }
@@ -45,6 +68,14 @@ namespace Project1
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(ball, ballLocation, null, Color.White, ballRotation,
+                new Vector2(ball.Width / 2, ball.Height / 2), 1, SpriteEffects.None, 0);
+            _spriteBatch.DrawString(_font, ballLocation.X + ", " + ballLocation.Y, Vector2.Zero, Color.Black);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
